7835d3319d82e188d364e1f18df6c73d99412b3c
Brett Salyer <brettsalyer@gmail.com>
2020-03-20 15:59:12 -0400

    Renamed ascii.h to defines.h

---
+++ input/defines.h
@@ -0,0 +1,54 @@
+/*
+    File: defines.h
+
+    Description: Includes type definitions for frequently used types
+        as well as definitions for ASCII and scan code characters and 
+        color codes in hexadecimal
+
+*/
+
+// Defines ASCII Codes in hexadecimal
+
+// Control codes
+#define NUL 0x00
+#define SOH 0x01
+#define STX 0x02
+#define ETX 0x03
+#define EOT 0x04
+#define ENQ 0x05
+#define ACK 0x06
+#define BEL 0x07
+#define BS  0x08
+#define HT  0x09
+#define LF  0x0A
+#define VT  0x0B
+#define FF  0x0C
+#define CR  0x0D
+#define SO  0x0E
+#define SI  0x0F
+#define DLE 0x10
+#define DC1 0x11
+#define DC2 0x12
+#define DC3 0x13
+#define DC4 0x14
+#define NAK 0x15
+#define SYN 0x16
+#define ETB 0x17
+#define CAN 0x18
+#define EM  0x19
+#define SUB 0x1A
+#define ESC 0x1B
+#define FS  0x1C
+#define GS  0x1D
+#define RS  0x1E
+#define US  0x1F
+
+// Scan codes for arrow keys
+#define LARROW_PRESS    0x4B
+#define LARROW_RELEASE  0xCB
+#define RARROW_PRESS    0x4D
+#define RARROW_RELEASE  0xCD
+#define UARROW_PRESS    0x48
+#define UARROW_RELEASE  0xC8
+#define DARROW_PRESS    0x50
+#define DARROW_RELEASE  0xD0    
