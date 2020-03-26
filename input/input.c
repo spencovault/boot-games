36cd3c898c4bb68749b22c5eb585dd30ab74d071
Brett Salyer <brettsalyer@gmail.com>
2020-03-26 12:02:07 -0400

    Renamed main.c to input.c

---
+++ input/input.c
@@ -0,0 +1,94 @@
+/**
+    @file input.h
+    @author Brett Salyer
+
+    @brief A library for handling input from the user via keyboard
+        and as well as validating it.
+
+*/
+
+#include "defines.h"
+#include "input.h"
+
+// Gets a single keystroke from the user. Returns as a scancode
+char get_keystroke_ascii()
+{
+    // A 16 bit unsigned integer for storing the 
+    // Least significant 16-bits of register EAX (AH & AL)
+    unsigned short int input_char;
+
+    __asm__("int $0x16"
+            : "=a" (input_char)
+            : "0" (0x0));
+
+    return ((char)input_char);
+}
+
+// Gets a single keystroke from the user. Returns an ASCII value.
+char get_keystroke_scancode()
+{
+    // A 16 bit unsigned integer for storing the 
+    // Least significant 16-bits of register EAX (AH & AL)
+    unsigned short int input_char;
+
+    __asm__("int $0x16"
+            : "=a" (input_char)
+            : "0" (0x0));
+
+    return ((char) (input_char >> 8));
+}
+
+// Checks the input keyboard value and compares it to another
+int check_input(char input, char comparing)
+{
+    if (input == comparing)
+    {
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+
+void on_keypress(char input, char key, void (*f)())
+{
+    if (check_input(input, key))
+    {
+        f();
+    }
+}
+
+// Prints a single character to the terminal (for testing)
+void print_keystroke(char outchar, unsigned short int attr)
+{
+    __asm__("int $0x10"
+            :
+            : "a" ((0x0e << 8) | outchar), "b" (attr));
+}
+
+void print_test()
+{
+    char outchar = 'G';
+    int attr = 0;
+    __asm__("int $0x10"
+            :
+            : "a" ((0x0e << 8) | outchar), "b" (attr));
+}
+
+// Testing done in main()
+void main(void) {
+
+    unsigned short int input;
+
+    while(1)
+    {
+        input = get_keystroke_scancode();
+
+        on_keypress(input, UARROW_PRESS, &print_test); 
+    }
+    
+
+    while (1) {
+        __asm__ ("hlt");
+    };
+}
