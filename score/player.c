ed1e27bdba7082bdba4005e2aea5b4d82cd9d194
ikbenhomer <ikbenhomer@gmail.com>
2020-03-26 20:28:58 -0400

    Added setter functions for Player structure variables

--- score/player.c
+++ score/player.c
@@ -11,17 +11,42 @@ void player_init(struct Player* player)
     player->highScore = 0;
 }
 
-char *get_initial(struct Player currPlayer)
+char get_initial(struct Player currPlayer)
 {
     return currPlayer.initial;
 }
 
-int *get_current_score(struct Player currPlayer)
+int get_current_score(struct Player currPlayer)
 {
     return currPlayer.currentScore;
 }
 
-int *get_high_score(struct Player currPlayer)
+int get_high_score(struct Player currPlayer)
 {
     return currPlayer.highScore;
+}
+
+void set_initial(char *playerChar, struct Player *player)
+{
+    for(int i = 0; playerChar[i] != 0; i++)
+    {
+        if (i > 3)
+        {
+            return;
+        }
+        player->initial[i] = playerChar[i];
+    }
+    return;
+}
+
+void set_current_score(int cScore, struct Player *player)
+{
+    player->currentScore = cScore;
+    return;
+}
+
+void set_high_score(int hScore, struct Player *player)
+{
+    player->highScore = hScore;
+    return;
 }
