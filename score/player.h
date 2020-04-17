89577c860d4238b8ce48621d5c99366f86419eb9
ikbenhomer <ikbenhomer@gmail.com>
2020-04-16 21:56:37 -0400

    Added Player structure function prototypes

--- score/player.h
+++ score/player.h
@@ -3,4 +3,12 @@ struct Player {
   char initial[3];
   int currentScore;
   int personalBest;
+  int highScore;
 };
+
+void player_init(struct Player* player);
+int get_current_score(struct Player currPlayer);
+int get_high_score(struct Player currPlayer);
+void set_initial(char *playerChar, struct Player *player);
+void set_current_score(int cScore, struct Player *player);
+void set_high_score(int hScore, struct Player *player);
