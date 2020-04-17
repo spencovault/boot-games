8e54b19ea226703b6357694a44377fd2a7758a0e
ikbenhomer <ikbenhomer@gmail.com>
2020-04-16 22:04:21 -0400

    Started implementing testing for player.c functions

--- score/main.c
+++ score/main.c
@@ -1,12 +1,11 @@
-void main(void) {
-    int i;
-    char s[] = {'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd'};
-    for (i = 0; i < sizeof(s); ++i) {
-        __asm__ (
-            "int $0x10" : : "a" ((0x0e << 8) | s[i])
-        );
-    }
-    while (1) {
-        __asm__ ("hlt");
-    };
+#include "player.h"
+#include <stdio.h>
+
+int main() 
+{
+    struct Player newPlayer;
+    player_init(&newPlayer);
+    printf("Current Score %i", newPlayer.currentScore);
+    
+    return 0;
 }
