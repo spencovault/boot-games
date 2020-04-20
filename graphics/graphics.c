8b560bf7b003ec0f5473304e185bd700c7e5a5bd
Nick Hirzel <nhirzel1@gmail.com>
2020-04-20 13:00:13 -0400

    adding some comments to graphics funcs

--- graphics/graphics.c
+++ graphics/graphics.c
@@ -38,6 +38,7 @@ void write_char(u8 character, u8 color)
 
 void write_str(char* str, u8 color)
 {
+    // walk through string until we reach null character
     while(*str != '\0')
     {
         write_char(*str, color);
@@ -47,6 +48,7 @@ void write_str(char* str, u8 color)
 
 void draw_line_x(u8 x_pos, u8 y_pos, u8 end_x, u8 sym, u8 color)
 {
+    // since all will be on the same line, only need to set_cursor_pos once
     set_cursor_pos(x_pos, y_pos);
     for(; x_pos < end_x; x_pos++)
     {
@@ -56,6 +58,7 @@ void draw_line_x(u8 x_pos, u8 y_pos, u8 end_x, u8 sym, u8 color)
 
 void draw_line_y(u8 x_pos, u8 y_pos, u8 end_y, u8 sym, u8 color)
 {
+    // need to set_cursor_pos because we're walking vertically downward
     for(; y_pos < end_y; y_pos++)
     {
         set_cursor_pos(x_pos, y_pos);
@@ -71,42 +74,43 @@ void draw_box(u8 x_pos, u8 y_pos, u8 w, u8 h, u8 color, int border)
     u8 y;
     for(y = y_pos; y <= end_y; y++)
     {
+        // starting a new line in the box
         set_cursor_pos(x_pos, y);
         u8 x;
         for(x = x_pos; x <= end_x; x++)
         {
             if(border)
             {
-                if(x == end_x && y == end_y)
+                if(x == end_x && y == end_y) // bottom right
                 {
                     write_char(188, color);
                 }
-                else if(x == x_pos && y == end_y)
+                else if(x == x_pos && y == end_y) // bottom left
                 {
                     write_char(200, color);
                 }
-                else if(x == end_x && y == y_pos)
+                else if(x == end_x && y == y_pos) // top right
                 {
                     write_char(187, color);
                 }
-                else if(x == x_pos && y == y_pos)
+                else if(x == x_pos && y == y_pos) // top left
                 {
                     write_char(201, color);
                 }
-                else if(x == x_pos || x == end_x)
+                else if(x == x_pos || x == end_x) // horizontal lines
                 {
                     write_char(186, color);
                 }
-                else if(y == y_pos || y == end_y)
+                else if(y == y_pos || y == end_y) // vertical lines
                 {
                     write_char(205, color);
                 }
-                else
+                else // fill
                 {
                     write_char(178, color);
                 }
             }
-            else
+            else // fill all if no border
             {
                 write_char(178, color);
             }
