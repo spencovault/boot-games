8cc75f08efbfe609d0e413338cc95542326fd4be
Brett Salyer <brettsalyer@gmail.com>
2020-04-22 10:15:45 -0400

    Added doxygen code documentation

--- input/input.h
+++ input/input.h
@@ -11,7 +11,26 @@
 
 #include "defines.h"
 
-
+/**
+ * Waits for user input and returns the input
+ * as an ASCII character
+ */
 char get_keystroke_ascii();
+
+/**
+ * Waits for the user input and returns the input
+ * as a scancode. This is used for capturing keystrokes 
+ * that are not representable with an ASCII character.
+ * 
+ */
 char get_keystroke_scancode();
+
+/**
+ * Compares the input the user has entered with
+ * some other character, whether that be ASCII
+ * or scan code. 
+ * 
+ * @param input the input the user as entered
+ * @param comparing the scan code or ASCII character that you wish to check for
+ */
 int check_input(char input, char comparing);
