ed281e03989128b8cfcf6d50ec4c06a2968aa7ba
ikbenhomer <ikbenhomer@gmail.com>
2020-04-25 19:05:11 -0400

    Added doxygen comments to player.h

--- score/player.h
+++ score/player.h
@@ -1,13 +1,19 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+/**
+ * Player C structure to allow for object oriented programming for player 
+ * objects that contain initials, current score, personal best, and high score parameters
+ */
 struct Player {
   char initial[3];
   int currentScore;
   int personalBest;
   int highScore;
 };
-
+/**
+ * Function signature to define functions in player.c 
+ */ 
 void player_init(struct Player* player);
 int get_current_score(struct Player currPlayer);
 int get_high_score(struct Player currPlayer);
