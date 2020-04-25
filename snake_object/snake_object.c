41b44f90987cc9fb5255ca51d083678a90b60631
Spencer Covault <spcovault@gmail.com>
2020-04-25 14:56:23 -0400

    Created a snake object and initialization method for the object

---
+++ snake_object/snake_object.c
@@ -0,0 +1,11 @@
+#include "snake_object.h"
+
+void snake_object_init(struct Snake * snake_object, int location_x, int location_y) {
+    int pos[1][1] = {{ location_x, location_y}};
+    snake_object->head_x = location_x;
+    snake_object->head_y = location_y; 
+    snake_object->length = 1;
+    snake_object->location = *pos;
+    snake_object->head_direction = 'e';
+
+}
