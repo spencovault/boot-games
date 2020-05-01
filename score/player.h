eac521e9c081f2efe98e18a901c1dbb3fa3a7cd8
ikbenhomer <ikbenhomer@gmail.com>
2020-04-30 20:36:51 -0400

    Edited the doxygen comments on player.c and player.h based on Nick's comments to be correctly displayed

--- score/player.h
+++ score/player.h
@@ -2,8 +2,16 @@
 #define PLAYER_H
 
 /**
- * Player C structure to allow for object oriented programming for player 
- * objects that contain initials, current score, personal best, and high score parameters
+ * @file player.h
+ *  
+ * @brief Includes player functions to track player initials and scores and update them 
+ * when necessary
+ * 
+ * @author Nathaniel Price
+ */
+/**
+ * Defines variables for the Player struct to be initialized and modified in player.c
+ * 
  */
 struct Player {
   char initial[3];
@@ -11,14 +19,55 @@ struct Player {
   int personalBest;
   int highScore;
 };
+
 /**
- * Function signature to define functions in player.c 
+ * Initializes player intials to empty char values and scores to zero when a new player struct
+ * is called
+ * @param player references the Player struct to initialize/create a new player
  */ 
 void player_init(struct Player* player);
+
+/**
+ * Gets the player's current score
+ * @param currPlayer references the current player for Player struct to be modified
+ * @return returns player currentScore value
+ */
 int get_current_score(struct Player currPlayer);
+
+/**
+ * Gets the player's high score
+ * @param currPlayer references the current player for Player struct to be modified
+ * @return returns player highScore value
+ */ 
 int get_high_score(struct Player currPlayer);
+
+/**
+ * Gets the player's initials
+ * @param currPlayer references the current player for Player stuct to be modified
+ * @return returns player's initials
+ */ 
+char get_initial(struct Player currPlayer);
+
+/**
+ * Sets the player's initials if called
+ * @param playerChar references initial in Player struct to be modified based on input of characters
+ * for initials
+ * @param player references specific Player struct to modify the player initials
+ */ 
 void set_initial(char *playerChar, struct Player *player);
+
+/**
+ * Sets player's current score if called
+ * @param cScore references currentScore in Player struct to be modified based on current score of game
+ * @param player references specific Player struct to modify the player's current score  
+ */
 void set_current_score(int cScore, struct Player *player);
+/**
+ * Sets player's high score if called
+ * @param hScore references highScore in player struct to be modified based on if the player has acheived
+ * a new high score
+ * @param player references specific Player struct to modify the player's high score
+ */ 
 void set_high_score(int hScore, struct Player *player);
 
 #endif
