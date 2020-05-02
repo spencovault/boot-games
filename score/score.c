83aabf672a375b591a36c7142e0c966ca362b63a
ikbenhomer <ikbenhomer@gmail.com>
2020-05-01 20:07:40 -0400

    Added point functions with return specified return types to update player->currentScore

--- score/score.c
+++ score/score.c
@@ -10,3 +10,27 @@ void score_init(struct Score *scoreStart)
     scoreStart->watermelon = WATERMELON;
     scoreStart->multiplier = MULTIPLIER;
 }
+
+int apple_point(struct Player *player, struct Score points)
+{
+    player->currentScore += points.apple;
+    return player->currentScore;
+}
+
+int cherry_point(struct Player *player, struct Score points)
+{
+    player->currentScore += points.cherry;
+    return player->currentScore;
+}
+
+int banana_point(struct Player *player, struct Score points)
+{
+    player->currentScore += points.banana;
+    return player->currentScore;
+}
+
+int watermelon_point(struct Player *player, struct Score points)
+{
+    player->currentScore += points.watermelon;
+    return player->currentScore;
+}
