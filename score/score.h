3dcc0f1782f847f0a0b61121ad13b1e2a97c1848
ikbenhomer <ikbenhomer@gmail.com>
2020-05-01 20:02:43 -0400

    Added function prototypes for point types

--- score/score.h
+++ score/score.h
@@ -1,3 +1,4 @@
+#include "player.h"
 #ifndef SCORE_H
 #define SCORE_H
 
@@ -9,15 +10,22 @@
 
 struct Score {
     int multiplier;
-    int appple;
+    int apple;
     int cherry;
     int banana;
     int watermelon; 
 };
+
 /**
  * Initializes scoring variables for game start
  * @param scoreStart refernces struct Score to initialize scoring values and multiplier
  */
 void score_init(struct Score *scoreStart);
+
+int apple_point(struct Player *player, struct Score points);
+int cherry_point(struct Player *player, struct Score points);
+int banana_point(struct Player *player, struct Score points);
+int watermelon_point(struct Player *player, struct Score points);
+
 
 #endif
